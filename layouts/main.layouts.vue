d8858442a3d451337ad792f0007c1af6c17158b5
EmmanuelDiomande <emmanuel.diomande@outlook.com>
2023-11-17 07:24:53 +0000

    Fixed content v1

--- layouts/main.layouts.vue
+++ layouts/main.layouts.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import Header from "@/components/views/headers/header.vue";
 import Footer from "@/components/views/footers/footer.vue";
+
+useHead({
+  link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+});
 </script>
 <template>
   <main class="flex flex-col text-gray-800">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,10 @@
 export default defineNuxtConfig({
    devtools: { enabled: true },
    css: ['~/assets/styles/main.css'],
-
    app: {
       head: {
          link: [
-          { rel: 'icon', type:'image/png', sizes: '32x32', href: '/logo/logo_carmen_security.png' },
+          { rel: 'icon', type:'image/png', href: 'favicon.png' },
             {
                rel: 'stylesheet',
                href: 'https://fonts.googleapis.com/css2?family=Figtree:wght@100;200;300;400;500;600;700;800;900&display=swap',
