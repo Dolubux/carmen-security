60692aad3a8d36c68f801bedcff1b54ed45aee7e
EmmanuelDiomande <emmanuel.diomande@outlook.com>
2024-03-29 21:51:54 +0000

    Add new Module - Teams

--- components/views/banners/banner.vue
+++ components/views/banners/banner.vue
@@ -3,7 +3,7 @@ import { PhoneIcon, EnvelopeIcon, MapPinIcon } from "@heroicons/vue/24/solid";
 </script>
 <template>
   <div
-    class="w-full relative flex flex-col md:h-[60vh] lg:h-[90vh] h-[60vh] items-center py-5"
+    class="w-full relative flex flex-col md:h-[60vh] lg:h-[90vh] h-[65vh] items-center py-5 font-mains"
   >
     <ViewsBannersCustomBanners />
 
@@ -31,13 +31,13 @@ import { PhoneIcon, EnvelopeIcon, MapPinIcon } from "@heroicons/vue/24/solid";
       <div class="flex m-auto justify-center gap-2">
         <NuxtLink
           to="/a-propos"
-          class="bg-orange-500 text-center py-3 px-4 rounded-full text-base md:text-xl lg:text-lg font-bold text-white font-mains"
+          class="bg-orange-500 text-center py-2 px-5 rounded-full text-base md:text-xl lg:text-lg font-bold text-white font-mains"
         >
           En savoir +
         </NuxtLink>
         <NuxtLink
           to="/contact"
-          class="flex items-center gap-1 border-[1px] border-orange-500 text-center text-base md:text-xl lg:text-lg py-3 px-4 rounded-full font-bold font-mains text-orange-500"
+          class="flex items-center gap-1 border-[1px] border-orange-500 text-center text-base md:text-xl lg:text-lg py-2 px-5 rounded-full font-bold font-mains text-orange-500"
         >
           <PhoneIcon class="h-4 md:h-7 lg:h-5" />
           Nous contactez
