dae8872e69d7054d8bf5c85632f6d2835029e725
Emmanuel Diomande <emmanuel.diomande@outlook.com>
2024-07-03 16:47:19 +0000

    2024-07-03 - 15.000Fcfa - Ajoute d'image - Redirect vue unique

--- components/views/services/proceted.services.vue
+++ components/views/services/proceted.services.vue
@@ -1,6 +1,40 @@
 <script setup>
 import { reactive } from "vue";
 import CardProcess from "@/components/views/process/card.process.vue";
+
+
+const Images = reactive([
+  '/images/services/news/cs_0.jpeg',
+  '/images/services/news/cs_1.jpeg',
+  '/images/services/news/cs_2.jpeg',
+  '/images/services/news/cs_3.jpeg',
+  '/images/services/news/cs_4.jpeg',
+  '/images/services/news/cs_5.jpeg',
+  '/images/services/news/cs_6.jpeg',
+  '/images/services/news/cs_7.jpeg',
+  '/images/services/news/cs_8.jpeg',
+  '/images/services/news/cs_9.jpeg',
+  // '/images/services/news/cs_10.jpeg',
+  '/images/services/news/cs_11.jpeg',
+  '/images/services/news/cs_12.jpeg',
+  '/images/services/news/cs_13.jpeg',
+  '/images/services/cs_0.jpg',
+  '/images/services/cs_1.jpg',
+  '/images/services/cs_2.jpg',
+  '/images/services/cs_3.jpg',
+  '/images/services/cs_4.jpg',
+  '/images/services/cs_5.jpg',
+  '/images/services/cs_6.jpg',
+  '/images/services/cs_7.jpg',
+  '/images/services/cs_8.jpg',
+  '/images/services/cs_9.jpg',
+  '/images/services/cs_10.jpg',
+  '/images/services/cs_11.jpg',
+  '/images/services/cs_12.jpg',
+  '/images/services/cs_13.jpg',
+  '/images/services/cs_14.jpg',
+
+])
 </script>
 <template>
   <div
@@ -16,136 +50,16 @@ import CardProcess from "@/components/views/process/card.process.vue";
   </div>
 
   <div class="grid grid-cols-2 md:grid-cols-4 gap-2 w-11/12 m-auto mt-8">
-    <div>
-      <NuxtImg
+    <div v-for="im in Images" >
+      <a :href="im" target="_blank">
+        <NuxtImg
         placeholder
         class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_4.jpg"
+        :src="im"
         alt=""
       />
+      </a>
     </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_1.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_2.jpg"
-        alt=""
-      />
-    </div>
-
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_12.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_3.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_5.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_0.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_13.jpg"
-        alt=""
-      />
-    </div>
-
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_6.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_7.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/person/cs_owner.png"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_1.jpg"
-        alt=""
-      />
-    </div>
-
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_9.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_10.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_11.jpg"
-        alt=""
-      />
-    </div>
-    <div>
-      <NuxtImg
-        placeholder
-        class="h-[220px] lg:h-[350px] object-cover w-full rounded-lg"
-        src="/images/services/cs_14.jpg"
-        alt=""
-      />
-    </div>
+   
   </div>
 </template>
